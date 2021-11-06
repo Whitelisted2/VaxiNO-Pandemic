f63e147ac403242c42165edf4e123a656ddfee38
i-was-here <nirmitarora26@gmail.com>
2021-11-06 16:03:44 +0530

    Daily updating stats page

---
+++ update_stat.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Updating Stats</title>
+</head>
+<body>
+    Updating Stats !!!
+    <?php
+    shell_exec("python stat_graph_generator.py");
+    header("location: stats.html");
+    ?>
+</body>
+</html>
