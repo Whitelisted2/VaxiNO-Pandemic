93ce0c6c85a14c251bcc71096952a9bae00de83d
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2021-11-06 20:21:24 +0530

    Map features modified

--- update_stat.php
+++ update_stat.php
@@ -9,10 +9,10 @@
 <body>
     Updating Stats !!!
     <?php
-    shell_exec("python stat_graph_generator.py");
+    /*shell_exec("python stat_graph_generator.py");
     shell_exec("python stat_graph_generator2.py");
-    shell_exec("python stat_graph_generator3.py");
-    header("location: stats.html");
+    shell_exec("python stat_graph_generator3.py");*/
+    header("location: stats_showing.php");
     ?>
 </body>
 </html>
