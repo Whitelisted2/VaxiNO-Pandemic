d8a88b25f9f74d1acca1375ff7a44053c62c439d
i-was-here <nirmitarora26@gmail.com>
2021-11-06 18:51:48 +0530

    Statistics part complete

--- update_stat.php
+++ update_stat.php
@@ -10,6 +10,8 @@
     Updating Stats !!!
     <?php
     shell_exec("python stat_graph_generator.py");
+    shell_exec("python stat_graph_generator2.py");
+    shell_exec("python stat_graph_generator3.py");
     header("location: stats.html");
     ?>
 </body>
