02c6e31ee0fd8759e3f32b4e0586ba46a3f0a71a
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2021-11-08 10:46:30 +0530

    signup.php and registered.html messages updated.

--- signup.php
+++ signup.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/most-basic.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,500&display=swap" rel="stylesheet">
+    
         <title>Signing You UP !</title>
         <style>
             body
@@ -14,6 +22,7 @@
         </style>
     </head>
     <body>
+    <div class="simple-box">
         <?php
         $servername = $_SERVER['SERVER_NAME'];
         $username="root";
@@ -35,11 +44,11 @@
         $result = mysqli_query($conn, $command);
 
         if($_POST["phnum"]<9999999 || $_POST["pincode"]<0 || $_POST["pincode"]>999999 || empty(trim($_POST["pass"])) || empty(trim($_POST["email"])) ||  empty(trim($_POST["name"]))){
-            die("PLEASE ENTER VALID CREDENTIALS !");
+            die("PLEASE ENTER VALID CREDENTIALS, &nbsp;<a href='user_login_signup.html'>try again</a>,&nbsp; !");
         }
 
         if($_POST["pass"] !== $_POST["repass"]){
-            die("Passwords were not same! please try again !");
+            die("Passwords were not same! please &nbsp;<a href='user_login_signup.html'>try again</a>,&nbsp;!");
         }
 
         $phnum_user=$_POST["phnum"];
@@ -72,16 +81,18 @@
         $sql="SELECT * FROM usertable WHERE phnum_user='$phnumcheck'";
         $result = mysqli_query($conn, $sql);
         if($row = mysqli_fetch_assoc($result)){
-            die("This Phone number is already registered !");
+            die("This Phone number is already registered. &nbsp;<a href='user_login_signup.html'>Try again</a>,&nbsp;!");
         }
 
         $command = "INSERT INTO usertable values ('$name_user', '$age', '$phnum_user', '$password_user', '$email_user', '$pincode_user', 0, 0, 0, 0)";
         $result = mysqli_query($conn, $command);
         if(!$result){
-            die("Record Not added ! Please try again. error : ".mysqli_error($conn));
+            die("Record Not added ! &nbsp;<a href='user_login_signup.html'>try again</a>,&nbsp; error : ".mysqli_error($conn));
         }
         
         header("location: registered.html");
         ?>
+
+    </div>
     </body>
 </html>
